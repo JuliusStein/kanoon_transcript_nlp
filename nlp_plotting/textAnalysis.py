148166805041630aa9f81259759f0b8be82dd814
Julius <juliusastein@gmail.com>
2022-10-05 11:14:14 -0400

    audio updates

--- nlp_plotting/textAnalysis.py
+++ nlp_plotting/textAnalysis.py
@@ -1,11 +1,15 @@
 import json
-
-casetexts = json.load(open('cases_with_text.json', 'r'))
+casetexts = json.load(open('../casetexts_all.json', 'r'))
+print("JSON loaded")
 alltexts = ""
 textlist = []
-for case in casetexts:
-    alltexts += case['full_text']+ "\n"
+length = len(casetexts)
+for num,case in enumerate(casetexts):
+    if((num+1)%100 == 0):
+        print(num+1,"of",length,"cases loaded")
+    #alltexts += case['full_text'] + "\n"
     textlist.append(case['full_text'])
+print("Texts loaded, analyzing...")
 
 #---------------
 import pandas as pd
@@ -28,16 +32,17 @@ from nltk.stem import WordNetLemmatizer
 # Ngrams allows to group words in common pairs or trigrams..etc
 from nltk import ngrams
 from nltk import Counter
-
 import seaborn as sns
 
 def word_frequency(sentences):
 # joins all the sentenses
+    customStopWords = ["sexual assault", "sexual", "assault", "perpetrator"]
     sentence = ' '.join(sentences)
     # creates tokens, creates lower class, removes numbers and lemmatizes the words
     new_tokens = word_tokenize(sentence)
     new_tokens = [t.lower() for t in new_tokens]
-    new_tokens =[t for t in new_tokens if t not in stopwords.words('english')]
+    stopwords = stopwords.words('english') + customStopWords
+    new_tokens =[t for t in new_tokens if t not in stopwords]
     new_tokens = [t for t in new_tokens if t.isalpha()]
     lemmatizer = WordNetLemmatizer()
     new_tokens =[lemmatizer.lemmatize(t) for t in new_tokens]
@@ -52,7 +57,6 @@ def word_frequency(sentences):
     return word_freq,word_pairs,trigrams
 
 freq, bi, tri = word_frequency(textlist)
-
 input("Continue to seaborn plots?")
 numResult = int(input("How many results do you want to plot?"))
 # create subplot of the different data frames
